8db31dd14c911abc134754969b4a5aa1aaba1ae4
Manuel DeJesus <69539934+ManuelLovell@users.noreply.github.com>
2023-09-28 23:30:05 -0700

    Added Disables

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,59 @@
 import OBR, { Image, Metadata } from '@owlbear-rodeo/sdk'
-import './style.css'
 import { Constants } from './constants';
+import * as Utilities from './utilities';
+import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    You found my secret space.  There's nothing to do here.
-    This extension basically is updated from one person - the GM's side. No reason for everyone to update things, right?
-    So just flip the button. Or bind stuff. Go away.
+    <div id="bannerText"></div>
+    <h4>Disable Z-Ordering:<input type="checkbox" id="disableZOrder"></h4>
+    <h4>Disable Binding for Players:<input type="checkbox" id="disableBinding"></h4>
   </div>
 `
+///Scrolling News
+const textArray = [
+    "Flip! v1.1",
+    "Added Disable for Z-Ordering",
+    "Added Disable for Binding"];
+
+let currentIndex = 0;
+let bindingDisabled = false;
+const textContainer = document.getElementById("bannerText")!;
+const zCheckbox = document.getElementById("disableZOrder")! as HTMLInputElement;
+const bindCheckbox = document.getElementById("disableBinding")! as HTMLInputElement;
+
+function fadeOut()
+{
+    textContainer.style.opacity = "0";
+    setTimeout(() =>
+    {
+        fadeIn();
+    }, 2000); // Fade-out time is 2 seconds
+}
+
+function fadeIn()
+{
+    currentIndex = (currentIndex + 1) % textArray.length;
+    textContainer.textContent = textArray[currentIndex];
+    textContainer.style.opacity = "1";
+    setTimeout(() =>
+    {
+        fadeOut();
+    }, 10000); // Fade-in time is 2 seconds
+}
+textContainer.textContent = textArray[currentIndex];
+fadeIn();
 
 OBR.onReady(async () =>
 {
+    // Set theme accordingly
+    const theme = await OBR.theme.getTheme();
+    Utilities.SetThemeMode(theme, document);
+    OBR.theme.onChange((theme) =>
+    {
+        Utilities.SetThemeMode(theme, document);
+    })
+
     const role = await OBR.player.getRole();
     // Do all setup through the GM role so we don't have multiple updates
     if (role == "GM")
@@ -33,6 +75,23 @@ OBR.onReady(async () =>
             else
             {
                 //Guess we'll just wait.
+            }
+        });
+    }
+    else
+    {
+        await OBR.action.setHeight(50);
+        document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Configuration is GM-Access only.`;
+        OBR.room.onMetadataChange(async (metadata) =>
+        {
+            bindingDisabled = metadata[`${Constants.EXTENSIONID}/disableBinding`] as boolean;
+            if (bindingDisabled)
+            {
+                await OBR.contextMenu.remove(Constants.CONTEXTBINDID);
+            }
+            if (bindingDisabled === false)
+            {
+                await SetupBindButton();
             }
         });
     }
@@ -157,153 +216,172 @@ OBR.onReady(async () =>
         }
     });
 
-    // Setup our BIND buttons
-    await OBR.contextMenu.create({
-        id: Constants.CONTEXTBINDID,
-        icons: [
-            {
-                icon: "/combine.svg",
-                label: "Bind It",
-                filter: {
-                    min: 2,
-                    max: 2,
-                    every: [
-                        // Only work on a pair at a time, with no bind metadata
-                        { key: ["metadata", `${Constants.EXTENSIONID}/metadata_bind`], value: undefined, coordinator: "&&" },
-                        { key: "layer", value: "MAP", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "GRID", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "DRAWING", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "ATTACHMENT", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "NOTE", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "TEXT", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "RULER", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "FOG", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "POINTER", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "CONTROL", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "POPOVER", operator: "!=", coordinator: "&&" },
-                    ],
-                },
-            },
-            {
-                icon: "/split.svg",
-                label: "UnBind It",
-                filter: {
-                    max: 1,
-                    every: [
-                        // Only unbind a set at a time, who has bind metadata
-                        { key: ["metadata", `${Constants.EXTENSIONID}/metadata_bind`], value: undefined, operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "MAP", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "GRID", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "DRAWING", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "ATTACHMENT", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "NOTE", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "TEXT", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "RULER", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "FOG", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "POINTER", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "CONTROL", operator: "!=", coordinator: "&&" },
-                        { key: "layer", value: "POPOVER", operator: "!=", coordinator: "&&" },
-                    ],
-                },
-            }
-        ],
-        async onClick(context, _: string)
-        {
-            if (context.items.length == 1)
-            {
-                // If it's one, this is an unbind
-                const item = context.items[0] as Image;
-
-                // Get it's metadata to find the mirror and update it to be seen
-                const metadata = item.metadata[`${Constants.EXTENSIONID}/metadata_bind`] as Metadata;
-                const mirrored = metadata.mirror as Mirror;
-                mirrored.x = item.position.x;
-                mirrored.y = item.position.y;
-
-                // Clear the bind metadata off so it's fresh
-                await OBR.scene.items.updateItems(
-                    (original) => original.id === item.id,
-                    (items) =>
-                    {
-                        for (let itemed of items)
-                        {
-                            itemed.metadata[`${Constants.EXTENSIONID}/metadata_bind`] = undefined;
-                        }
-                    });
-
-                // Make the mirrored pair visible and slightly offset from the original, based on the original's potition
-                // Also clear that metadata
-                await OBR.scene.items.updateItems(
-                    (item) => item.id === mirrored.id,
-                    (items) =>
-                    {
-                        for (let itemed of items)
-                        {
-                            itemed.image.height = mirrored.height;
-                            itemed.image.width = mirrored.width;
-                            itemed.position.x = mirrored.x! + (item.image.width / 2);
-                            itemed.position.y = mirrored.y! + (item.image.height / 4);
-                            itemed.disableHit = false;
-                            itemed.locked = false;
-                            itemed.metadata[`${Constants.EXTENSIONID}/metadata_bind`] = undefined;
-                        }
-                    });
-            }
-            else
-            {
-                // If it's not a single, we're binding
-                await OBR.scene.items.updateItems(context.items, (items) =>
+    async function SetupBindButton(): Promise<void>
+    {
+        // Setup our BIND buttons
+        await OBR.contextMenu.create({
+            id: Constants.CONTEXTBINDID,
+            icons: [
                 {
-                    /// Create copies of the items so we don't reference values
-                    const first = { ...items[0] };
-                    const second = { ...items[1] };
+                    icon: "/combine.svg",
+                    label: "Bind It",
+                    filter: {
+                        min: 2,
+                        max: 2,
+                        every: [
+                            // Only work on a pair at a time, with no bind metadata
+                            { key: ["metadata", `${Constants.EXTENSIONID}/metadata_bind`], value: undefined, coordinator: "&&" },
+                            { key: "layer", value: "MAP", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "GRID", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "DRAWING", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "ATTACHMENT", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "NOTE", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "TEXT", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "RULER", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "FOG", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "POINTER", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "CONTROL", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "POPOVER", operator: "!=", coordinator: "&&" },
+                        ],
+                    },
+                },
+                {
+                    icon: "/split.svg",
+                    label: "UnBind It",
+                    filter: {
+                        max: 1,
+                        every: [
+                            // Only unbind a set at a time, who has bind metadata
+                            { key: ["metadata", `${Constants.EXTENSIONID}/metadata_bind`], value: undefined, operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "MAP", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "GRID", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "DRAWING", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "ATTACHMENT", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "NOTE", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "TEXT", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "RULER", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "FOG", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "POINTER", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "CONTROL", operator: "!=", coordinator: "&&" },
+                            { key: "layer", value: "POPOVER", operator: "!=", coordinator: "&&" },
+                        ],
+                    },
+                }
+            ],
+            async onClick(context, _: string)
+            {
+                if (context.items.length == 1)
+                {
+                    // If it's one, this is an unbind
+                    const item = context.items[0] as Image;
 
-                    for (let index = 0; index < items.length; index++)
-                    {
-                        // Take a mirror of the ID, height and width so we can zero out the mirrored
-                        const mirror: Mirror = index === 0 ?
-                            { id: second.id, height: second.image.height, width: second.image.width }
-                            : { id: first.id, height: first.image.height, width: first.image.width };
+                    // Get it's metadata to find the mirror and update it to be seen
+                    const metadata = item.metadata[`${Constants.EXTENSIONID}/metadata_bind`] as Metadata;
+                    const mirrored = metadata.mirror as Mirror;
+                    mirrored.x = item.position.x;
+                    mirrored.y = item.position.y;
 
-                        items[index].metadata[`${Constants.EXTENSIONID}/metadata_bind`] = { mirror };
-
-                        // Update the mirror to be hidden
-                        if (index === 1)
+                    // Clear the bind metadata off so it's fresh
+                    await OBR.scene.items.updateItems(
+                        (original) => original.id === item.id,
+                        (items) =>
                         {
-                            items[1].image.height = 1;
-                            items[1].image.width = 1;
-                            items[1].position.x = 1;
-                            items[1].position.y = 1;
-                            items[1].disableHit = true;
-                            items[1].locked = true;
-                        }
-                    }
-                });
-                // Select the first of the array since we hid the other guy
-                await OBR.player.select([context.items[0].id]);
-            }
-        },
-    });
+                            for (let itemed of items)
+                            {
+                                itemed.metadata[`${Constants.EXTENSIONID}/metadata_bind`] = undefined;
+                            }
+                        });
 
+                    // Make the mirrored pair visible and slightly offset from the original, based on the original's potition
+                    // Also clear that metadata
+                    await OBR.scene.items.updateItems(
+                        (item) => item.id === mirrored.id,
+                        (items) =>
+                        {
+                            for (let itemed of items)
+                            {
+                                itemed.image.height = mirrored.height;
+                                itemed.image.width = mirrored.width;
+                                itemed.position.x = mirrored.x! + (item.image.width / 2);
+                                itemed.position.y = mirrored.y! + (item.image.height / 4);
+                                itemed.disableHit = false;
+                                itemed.locked = false;
+                                itemed.metadata[`${Constants.EXTENSIONID}/metadata_bind`] = undefined;
+                            }
+                        });
+                }
+                else
+                {
+                    // If it's not a single, we're binding
+                    await OBR.scene.items.updateItems(context.items, (items) =>
+                    {
+                        /// Create copies of the items so we don't reference values
+                        const first = { ...items[0] };
+                        const second = { ...items[1] };
+
+                        for (let index = 0; index < items.length; index++)
+                        {
+                            // Take a mirror of the ID, height and width so we can zero out the mirrored
+                            const mirror: Mirror = index === 0 ?
+                                { id: second.id, height: second.image.height, width: second.image.width }
+                                : { id: first.id, height: first.image.height, width: first.image.width };
+
+                            items[index].metadata[`${Constants.EXTENSIONID}/metadata_bind`] = { mirror };
+
+                            // Update the mirror to be hidden
+                            if (index === 1)
+                            {
+                                items[1].image.height = 1;
+                                items[1].image.width = 1;
+                                items[1].position.x = 1;
+                                items[1].position.y = 1;
+                                items[1].disableHit = true;
+                                items[1].locked = true;
+                            }
+                        }
+                    });
+                    // Select the first of the array since we hid the other guy
+                    await OBR.player.select([context.items[0].id]);
+                }
+            },
+        });
+    }
+
+    await SetupBindButton();
     // Do our scene setup which gives everyone a base anchor for Z-axis ordering
     async function SetupFlip(): Promise<void>
     {
+        zCheckbox.addEventListener("click", async (event: MouseEvent) =>
+        {
+            const target = event.target as HTMLInputElement;
+            await OBR.room.setMetadata({ [`${Constants.EXTENSIONID}/disableZIndex`]: target.checked });
+        }, false);
+
+        bindCheckbox.addEventListener("click", async (event: MouseEvent) =>
+        {
+            const target = event.target as HTMLInputElement;
+            await OBR.room.setMetadata({ [`${Constants.EXTENSIONID}/disableBinding`]: target.checked });
+        }, false);
+
         await OBR.scene.items.onChange(async (itemsChanged) =>
         {
             await OBR.scene.items.updateItems(itemsChanged, (items) =>
             {
-                for (let item of items)
+                if (!zCheckbox.checked)
                 {
-                    // We're only dealing with Image data, so if it's not an image go away
-                    if (item.type !== "IMAGE") continue;
-                    // To give us a decent range to play in we multiply the position by alot and then work with the height
-                    const anchor = (item.position.y * 1000) + (item.image.height / 6);
-                    if ((item.zIndex !== anchor
-                        && item.metadata[`${Constants.EXTENSIONID}/metadata_flip`] !== undefined)
-                        || item.metadata[`${Constants.EXTENSIONID}/metadata_flip`] === undefined)
+                    for (let item of items)
                     {
-                        item.metadata[`${Constants.EXTENSIONID}/metadata_flip`] = { anchor };
-                        item.zIndex = anchor;
+                        // We're only dealing with Image data, so if it's not an image go away
+                        if (item.type !== "IMAGE") continue;
+                        // To give us a decent range to play in we multiply the position by alot and then work with the height
+                        const anchor = (item.position.y * 1000) + (item.image.height / 6);
+                        if ((item.zIndex !== anchor
+                            && item.metadata[`${Constants.EXTENSIONID}/metadata_flip`] !== undefined)
+                            || item.metadata[`${Constants.EXTENSIONID}/metadata_flip`] === undefined)
+                        {
+                            item.metadata[`${Constants.EXTENSIONID}/metadata_flip`] = { anchor };
+                            item.zIndex = anchor;
+                        }
                     }
                 }
             });
